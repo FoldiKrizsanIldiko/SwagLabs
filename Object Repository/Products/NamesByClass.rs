6b4e206e03465e65ddb0ce02d00250f20626fb50
FoldiKrizsanIldiko <fkildiko13@gmail.com>
2024-04-10 08:06:15 +0000

    feat: print product names from product page

---
+++ Object Repository/Products/NamesByClass.rs
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>NamesByClass</name>
+   <tag></tag>
+   <elementGuidId>d0e4862b-18be-464f-a58c-20eb06ba573b</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class='inventory_item_name ']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>inventory_item_name </value>
+      <webElementGuid>89cf4198-1c3f-4115-9429-a765c5ea38b6</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
